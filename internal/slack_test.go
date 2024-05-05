a7d833b46811d616a2e767de55d9fab67144294c
Shayan Sadeghieh <shayan.sadeghieh@gmail.com>
2024-05-05 13:05:29 -0400

    Introduce internal package

---
+++ internal/slack_test.go
@@ -0,0 +1,46 @@
+package internal
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestExtractNames(t *testing.T) {
+	testCases := []struct {
+		name    string
+		members AllMembers
+		exp     []string
+	}{
+		{
+			name: "Multiple members",
+			members: AllMembers{
+				Members: []Member{
+					{Profile: Profile{RealNameNormalized: "shayan sadeghieh"}},
+					{Profile: Profile{RealNameNormalized: "foo bar"}},
+					{Profile: Profile{RealNameNormalized: "foo"}},
+				},
+			},
+			exp: []string{"shayan sadeghieh", "foo bar", "foo"},
+		},
+		{
+			name: "No members",
+			members: AllMembers{
+				Members: []Member{},
+			},
+			exp: []string{},
+		},
+	}
+
+	// Iterate over test cases and run them
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			// Run the function being tested
+			names := extractNames(tc.members)
+			// Check if the output matches the expected result
+			if !reflect.DeepEqual(names, tc.exp) {
+				t.Errorf("Expected %v, but got %v", tc.exp, names)
+			}
+		})
+	}
+
+}
--- main.go
+++ main.go
@@ -1,87 +1,26 @@
 package main
 
 import (
-	"bufio"
-	"bytes"
-	"encoding/json"
 	"fmt"
 	"log"
-	"net/http"
 	"os"
-	"os/exec"
-	"strings"
+	"prposter/internal"
 )
 
-type PullRequest struct {
-	URL       string `json:"url"`
-	Number    int    `json:"number"`
-	Additions int    `json:"additions"`
-	Deletions int    `json:"deletions"`
-}
-
-func sendSlackMessage(message string) error {
-	token := os.Getenv("SLACK_API_TOKEN")
-	if token == "" {
-		return fmt.Errorf("SLACK_API_TOKEN environment variable is not set")
-	}
-
-	// Slack API URL
-	url := "https://slack.com/api/chat.postMessage"
-
-	// Create HTTP request
-	req, err := http.NewRequest("POST", url, bytes.NewBuffer([]byte(message)))
-	if err != nil {
-		return err
-	}
-
-	// Set request headers
-	req.Header.Set("Content-Type", "application/json")
-	req.Header.Set("Authorization", "Bearer "+token)
-
-	// Send HTTP request
-	client := &http.Client{}
-	resp, err := client.Do(req)
-	if err != nil {
-		return err
-	}
-	defer resp.Body.Close()
-
-	// Check response status
-	if resp.StatusCode != http.StatusOK {
-		return fmt.Errorf("slack API request failed with status: %s", resp.Status)
-	}
-
-	return nil
-}
-
-func stringPrompt(label string) string {
-	var s string
-	r := bufio.NewReader(os.Stdin)
-	for {
-		fmt.Fprint(os.Stderr, label+" ")
-		s, _ = r.ReadString('\n')
-		if s != "" {
-			break
-		}
-	}
-	return strings.TrimSpace(s)
-}
-
 func main() {
-	cmd := exec.Command("gh", "pr", "view", "--json", "url,number,additions,deletions")
-	output, err := cmd.Output()
+	names, err := internal.ReadSlackUsers()
 	if err != nil {
-		log.Fatalf("Error running gh pr view command: %v", err)
+		log.Fatalf("Error reading slack users: %v", err)
 	}
+	fmt.Println("names are", names)
 
-	var pr PullRequest
-	err = json.Unmarshal([]byte(output), &pr)
-
+	pr, err := internal.GhCommand()
 	if err != nil {
-		log.Fatalf("Error unmarshaling output from gh pr view command: %v", err)
+		log.Fatalf("Error running gh command: %v", err)
 	}
+	fmt.Println("pr is", pr)
 
-	prDescription := stringPrompt(fmt.Sprintf("Provide a description for PR #%s:", fmt.Sprint(pr.Number)))
+	prDescription := internal.StringPrompt(fmt.Sprintf("Provide a description for PR #%s:", fmt.Sprint(pr.Number)))
 
 	// Create the message format for Slack
 	messageFormat := `{"channel": "%s", "text": "(+%s/-%s) <%s|PR #%s>: %s"}`
@@ -92,8 +31,9 @@ func main() {
 	}
 	message := fmt.Sprintf(messageFormat, channelID, fmt.Sprint(pr.Additions), fmt.Sprint(pr.Deletions), pr.URL, fmt.Sprint(pr.Number), prDescription)
 
-	err = sendSlackMessage(message)
+	err = internal.SendSlackMessage(message)
 	if err != nil {
 		log.Fatalf("Error sending message to Slack: %v", err)
 	}
+
 }
